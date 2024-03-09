cb5345f9e0d6f1c8e8124c13a97165d6557a1b87
Akihiko Nakamura <anakamuro@earthlink.net>
2024-03-08 20:43:59 -0500

    second commit

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
    for(let i = 0; i < 50; i++){
     const randomCityIndex = Math.floor(Math.random() * cities.length);
+    const price = Math.floor(Math.random() * 2000) + 1000;
     const camp = new Campground({
         location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
-        title: `${sample(descriptors)} . ${sample(places)}`
+        title: `${sample(descriptors)} . ${sample(places)}`, 
+        image: 'https://source.unsplash.com/collection/483251', 
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+        price
     });
     await camp.save();
    }
